a7e4cce2051e86548205095fe8d32e94f981970c
abdulayef1 <7ka1cyr@code.edu.az>
2023-03-31 14:23:33 +0400

    Collection layer is created

---
+++ TestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿LinkedList<int> list=new LinkedList<int>();
+list.AddFirst(0);
+list.AddFirst(1);
+list.AddFirst(2);
+list.AddFirst(3);
+list.AddLast(4);
+list.Clear();
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
